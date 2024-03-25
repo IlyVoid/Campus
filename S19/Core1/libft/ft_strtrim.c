7eb25cac9a175d34e2af916aa75c8584507e8373
IlyVoid <voidschp@gmail.com>
2024-03-25 13:28:07 +0000

    commiting additional functions

---
+++ S19/Core1/libft/ft_strtrim.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-03-20 12:09:36 by user              #+#    #+#             */
+/*   Updated: 2024-03-20 12:09:36 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+	int		start;
+	int		end;
+	char	*newstr;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	len = end - start + 1;
+	newstr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!newstr)
+		return (NULL);
+	ft_strlcpy(newstr, s1 + start, len + 1);
+	return (newstr);
+}
