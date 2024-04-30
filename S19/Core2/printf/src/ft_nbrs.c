9c900de44ca730ec789d6f5fee63d6aa4a8d0977
Quinten Van Den Berghe <quvan-de@a2-r2-p9.s19.be>
2024-04-30 10:58:58 +0200

    handles when no str

---
+++ S19/Core2/printf/src/ft_nbrs.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nbrs.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: quvan-de <quvan-de@student.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 11:55:37 by quvan-de          #+#    #+#             */
+/*   Updated: 2024/04/22 14:10:15 by quvan-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putnbr_printf(int n, size_t *counter)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_printf("-2147483648", counter);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_printf('-', counter);
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr_printf((n / 10), counter);
+	ft_putchar_printf(('0' + n % 10), counter);
+}
+
+void	ft_putuint_printf(unsigned int n, size_t *counter)
+{
+	if (n > 9)
+		ft_putuint_printf((n / 10), counter);
+	ft_putchar_printf(('0' + n % 10), counter);
+}
