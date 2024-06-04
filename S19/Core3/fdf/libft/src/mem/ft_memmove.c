012abc7f1e0fb3118ae0aa8683c448b218075c85
Quinten Van Den Berghe <quvan-de@a1-r2-p8.s19.be>
2024-06-04 17:45:09 +0200

    commit

---
+++ S19/Core3/fdf/libft/src/mem/ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: quvan-de <quvan-de@student.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 17:34:47 by quvan-de          #+#    #+#             */
+/*   Updated: 2024/06/04 17:34:50 by quvan-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	size_t	len;
+
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	if (src < dst)
+	{
+		len = n;
+		while (len > 0)
+		{
+			len--;
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+		}
+	}
+	else
+	{
+		len = 0;
+		while (len < n)
+		{
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+			len++;
+		}
+	}
+	return (dst);
+}
