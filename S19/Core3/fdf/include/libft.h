6cfd2d822513265b3a0d37a46e87712012c55cfa
void <161216891+IlyVoid@users.noreply.github.com>
2024-06-19 12:39:26 +0100

    adding init_hooks

--- S19/Core3/fdf/include/libft.h
+++ S19/Core3/fdf/include/libft.h
@@ -27,6 +27,7 @@ int					ft_isdigit(int i);
 int					ft_isascii(int i);
 int					ft_isprint(int i);
 int                 ft_isspace(char c);
+int                 ft_imax(int a, int b);
 int					ft_toupper(int i);
 int					ft_tolower(int i);
 void				*ft_memset(void *b, int c, size_t len);
---
+++ S19/Core3/fdf/libft/src/checkers/ft_imax.c
@@ -0,0 +1,6 @@
+int	ft_imax(int a, int b)
+{
+    if (a > b)
+        return (a);
+    return (b);
+}
