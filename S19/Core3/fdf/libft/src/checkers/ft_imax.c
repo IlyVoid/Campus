cb53d51247360f403ac201cb5cb703bc7aeab630
Quinten Van Den Berghe <quvan-de@a1-r2-p5.s19.be>
2024-06-20 15:37:04 +0200

    commit

--- S19/Core3/fdf/libft/src/checkers/ft_imax.c
+++
@@ -1,6 +0,0 @@
-int	ft_imax(int a, int b)
-{
-    if (a > b)
-        return (a);
-    return (b);
-}
--- S19/Core3/fdf/libft/src/checkers/ft_isspace.c
+++
@@ -1,9 +0,0 @@
-#include "../../../include/libft.h"
-
-int	ft_isspace(char c)
-{
-    if (c == ' ' || c == '\t' || c == '\n'
-        || c == '\v' || c == '\f' || c == '\r')
-        return (1);
-    return (0);
-}
