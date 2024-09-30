54bf7d9f3e49bcebe86240ab1beb2508371d5844
void <voidschp@gmail.com>
2024-09-30 23:27:28 +0100

    cpp00 ex00 done, ex01 started

--- S19/Core5/CPP00/ex01/Contact.hpp
+++ S19/Core5/CPP00/ex01/Contact.hpp
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: quvan-de <quvan-de@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/30 23:13:33 by quvan-de          #+#    #+#             */
-/*   Updated: 2024/09/30 23:20:15 by quvan-de         ###   ########.fr       */
+/*   Created: 2024/09/30 23:23:17 by quvan-de          #+#    #+#             */
+/*   Updated: 2024/09/30 23:26:55 by quvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cctype>
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
-int main(int argc, char **argv) {
-	if (argc == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" << std::endl;
-	} else {
-		for (int i = 1; i < argc; i++) {
-			for (int j = 0; argv[i][j] != '\0'; j++) {
-				std::cout << (char)toupper(argv[i][j]);
-			}
-			if (i < argc - 1)
-				std::cout << " ";
-		}
-		std::cout << std::endl;
-	}
-	return 0;
+#include <string>
+
+class Contact {
+private:
+	std:: string firstName;
+	std:: string lastName;
+	std:: string nickname;
+	std:: string phoneNumber;
+	std:: string darkestSectret;
+
+public:
+	//setters
+	void setFirstName(std:: string firstName);
+	void setLastName(std:: string lastName);
 }
